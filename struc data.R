18945006a24225017ab7e0bdb5b56a8f9452327d
xMethlinG <andreas.methling@gmail.com>
2021-11-24 15:11:26 +0100

    færdig

--- struc data.R
+++ struc data.R
@@ -1,56 +1,62 @@
 library(readr)
 library(tidyverse)
-structures <- read_csv("C:/Users/andre/Desktop/structures.csv")
+structures <- read_csv("https://raw.githubusercontent.com/simonmig10/M3/main/structures.csv")
 View(structures)
 
 min5 = structures %>% filter(Time <= 5) %>%
   group_by(Address) %>%
   count(Team) %>%
   pivot_wider(names_from = Team, values_from = n, values_fill = 0) %>%
-  mutate(tower_diff = bTowers - rTowers) %>% 
-  select(Address, tower_diff) %>% ungroup
+  mutate(tower_diff_5 = bTowers - rTowers) %>% 
+  select(Address, tower_diff_5) %>% ungroup
 
 
 min10 = structures %>% filter(Time <= 10) %>%
   group_by(Address) %>%
   count(Team) %>%
   pivot_wider(names_from = Team, values_from = n, values_fill = 0) %>%
-  mutate(tower_diff = bTowers - rTowers) %>% 
-  select(Address, tower_diff) %>% ungroup
+  mutate(tower_diff_10 = bTowers - rTowers) %>% 
+  select(Address, tower_diff_10) %>% ungroup
 
 
 min15 = structures %>% filter(Time <= 15) %>%
   group_by(Address) %>%
   count(Team) %>%
   pivot_wider(names_from = Team, values_from = n, values_fill = 0) %>%
-  mutate(tower_diff = bTowers - rTowers) %>% 
-  select(Address, tower_diff) %>% ungroup
+  mutate(tower_diff_15 = bTowers - rTowers) %>% 
+  select(Address, tower_diff_15) %>% ungroup
 
 
-min20 = structures %>% filter(Time <= 20) %>%
+min5_in = structures %>% filter(Time <= 5) %>%
   group_by(Address) %>%
   count(Team) %>%
   pivot_wider(names_from = Team, values_from = n, values_fill = 0) %>%
-  mutate(tower_diff = bTowers - rTowers) %>% 
-  select(Address, tower_diff) %>% ungroup
+  mutate(inhib_diff_5 = bInhibs - rInhibs) %>% 
+  select(Address, inhib_diff_5) %>% ungroup
 
-min25 = structures %>% filter(Time <= 25) %>%
+
+min10_in = structures %>% filter(Time <= 10) %>%
+  group_by(Address) %>%
+  count(Team) %>%
+  pivot_wider(names_from = Team, values_from = n) %>% 
+  mutate(inhib_diff_10 = bInhibs - rInhibs) %>% 
+  select(Address, inhib_diff_10) %>% ungroup
+
+
+min15_in = structures %>% filter(Time <= 15) %>%
   group_by(Address) %>%
   count(Team) %>%
   pivot_wider(names_from = Team, values_from = n, values_fill = 0) %>%
-  mutate(tower_diff = bTowers - rTowers) %>% 
-  select(Address, tower_diff) %>% ungroup
+  mutate(inhib_diff_15 = bInhibs - rInhibs) %>% 
+  select(Address, inhib_diff_15) %>% ungroup
 
-min30 = structures %>% filter(Time <= 30) %>%
-  group_by(Address) %>%
-  count(Team) %>%
-  pivot_wider(names_from = Team, values_from = n, values_fill = 0) %>%
-  mutate(tower_diff = bTowers - rTowers) %>% 
-  select(Address, tower_diff) %>% ungroup
-
-min30a = structures %>% filter(Time > 30) %>%
-  group_by(Address) %>%
-  count(Team) %>%
-  pivot_wider(names_from = Team, values_from = n, values_fill = 0) %>%
-  mutate(tower_diff = bTowers - rTowers) %>% 
-  select(Address, tower_diff) %>% ungroup
+total_struc = structures %>% 
+  count(Address) %>% 
+  select(Address) %>% 
+  left_join(min5, by = c("Address")) %>% 
+  left_join(min10, by = c("Address")) %>% 
+  left_join(min15, by = c("Address")) %>% 
+  mutate(inhib_diff_5 = 0) %>% 
+  mutate(inhib_diff_10 = 0) %>% 
+  left_join(min15_in, by = c("Address")) %>% 
+  replace_na(list(tower_diff_5 = 0, tower_diff_10 = 0, tower_diff_15 = 0, inhib_diff_15 = 0))
